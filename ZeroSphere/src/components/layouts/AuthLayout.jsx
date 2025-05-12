d5d019ab75b3935cfb1e877f9f97cb9aae2bd2c2
bhavini-lakhotia <bhavinilakhotia@gmail.com>
2025-05-13 03:12:20 +0530

    updated templates

--- ZeroSphere/src/components/layouts/AuthLayout.jsx
+++ ZeroSphere/src/components/layouts/AuthLayout.jsx
@@ -10,7 +10,7 @@ const AuthLayout = ({children}) => {
 
         <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
             <div className='w-48 h-48 bg-purple-600 absolute -top-7 -left-5'></div>
-            <div className='w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10'></div>
+            <div className='w-48 h-56 rounded-[40px] border-20 border-fuchsia-600 absolute top-[30%] -right-10'></div>
             <div className='w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5'></div>
 
             <div className='grid grid-cols-1 z-20'>
